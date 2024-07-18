5fe0354bcc108157a184fe3309def4845f8e25de
Jakob <jakob.d.neuhauser@gmail.com>
2024-07-18 12:24:43 +0200

    picture added

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from general_classes.Simulation import Simulation
 
 my_sim = Simulation(Option())
 
-#Option().plot_random_stock_price_path(100)
+Option().plot_random_stock_price_path(100)
 
-#print(my_sim.calculate_analytic())
+print(my_sim.calculate_analytic())
 
-#my_sim.plot_trinomial_tree(1, 10)
+my_sim.plot_trinomial_tree(1, 10)
 my_sim.plot_explicit_fd(1, 40, 1, 20, 'n')
 my_sim.plot_implicit_fd(1, 40, 1, 20, 'n')
 my_sim.plot_crank_nicolson(1, 40, 1, 20, 'n')
